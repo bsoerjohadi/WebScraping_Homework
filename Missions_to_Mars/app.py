c6678e7a86837ee631df05db0d5905150dc0d397
bsoerjohadi <bsoerjohadi@ucdavis.edu>
2020-10-08 15:16:49 -0700

    upload homework

--- Missions_to_Mars/app.py
+++ Missions_to_Mars/app.py
@@ -6,7 +6,7 @@ import scrape_mars
 app = Flask(__name__)
 
 # Use PyMongo to establish Mongo connection
-mongo = PyMongo(app, uri="mongodb://localhost:27017/weather_app")
+mongo = PyMongo(app, uri="mongodb://localhost:27017/app")
 
 # Route to render index.html template using data from Mongo
 @app.route("/")
@@ -16,17 +16,19 @@ def mars():
     mars_data = mongo.db.collection.find_one()
 
     # Return template and data
-    return render_template("index.html", mars=mars_data)
+    return render_template("index.html", mars_info=mars_data)
 
 # Route that will trigger the scrape function
 @app.route("/scrape")
 def scrape():
 
+    listings = mongo.db.collection
+
     # Run the scrape function
     mars_info = scrape_mars.scrape()
 
     # Update the Mongo database using update and upsert=True
-    mongo.db.collection.update({}, mars_info, upsert=True)
+    listings.update({}, mars_info, upsert=True)
 
     # Redirect back to home page
     return redirect("/")
--- Missions_to_Mars/scrape_mars.py
+++ Missions_to_Mars/scrape_mars.py
@@ -7,11 +7,12 @@ import time
 import re
 
 def scrape():
-    executable_path = {'executable_path': 'chromedriver.exe'}
+    executable_path = {'executable_path': '/usr/local/bin/chromedriver'}
     browser = Browser('chrome', **executable_path, headless=False)
 
     url = 'https://mars.nasa.gov/news/'
     browser.visit(url)
+    time.sleep(10)
     news_html = browser.html
     soup = BeautifulSoup(news_html, "html.parser")
 
@@ -20,10 +21,12 @@ def scrape():
 
     image_url = 'https://www.jpl.nasa.gov/spaceimages/?search=&category=Mars'
     browser.visit(image_url)
+    time.sleep(10)
     image_html = browser.html
     image_soup = BeautifulSoup(image_html, "html.parser")
 
     base_url = 'https://www.jpl.nasa.gov'
+    time.sleep(10)
     featured_image_url = image_soup.find('img', class_ = 'thumb')["src"]
     featured_image = base_url + featured_image_url
 
@@ -34,13 +37,22 @@ def scrape():
     weather_soup = BeautifulSoup(weather_html, "html.parser") 
 
     tweets = weather_soup.find_all("span",text=re.compile('InSight sol'))
-    latest_weather=tweets[0].get_text()
+    latest_tweets=tweets[0].get_text()
 
+    # Mars Facts
     facts_url = 'https://space-facts.com/mars/'
-    tables = pd.read_html(facts_url)
+    browser.visit(facts_url)
+    time.sleep(10)
+
+    facts_tables = pd.read_html(facts_url)
+    facts_df = facts_tables[0]
+    facts_df.columns = ["Description", "Value"]
+    facts_df = facts_df.set_index("Description")
+    html_table = facts_df.to_html()
 
     hemispheres_url = 'https://astrogeology.usgs.gov/search/results?q=hemisphere+enhanced&k1=target&v1=Mars'
     browser.visit(hemispheres_url)
+    time.sleep(10)
     hem_html = browser.html
     hemispheres_soup = BeautifulSoup(hem_html, "html.parser")
 
@@ -54,11 +66,15 @@ def scrape():
             title = hemisphere.find('h3').text
 
             # scrape the article image url
-            hem_url = hemisphere.find('img', class_='thumb')['src']
+            img_url = hemisphere.find('a', class_= 'itemLink product-item')['href']
+            browser.visit('https://astrogeology.usgs.gov' + img_url)
+            time.sleep(10)
+            img_url = browser.html
+            imgs_url = hemisphere.find('img', class_='thumb')['src']
 
             # Print results only if title, price, and link are available
-            if (title and hem_url):
-                hem_urls  = hemispheres_url + hem_url
+            if (title and imgs_url):
+                hem_urls  = 'https://astrogeology.usgs.gov' + imgs_url
                 links.append({'title': title, 'img_urls': hem_urls})
 
     # Store all in dictionary
@@ -66,9 +82,9 @@ def scrape():
         "news_title": news_title,
         "news_summary": news_p, 
         "featured_image": featured_image,
-        "weather": latest_weather,
-        "facts": tables,
-        "hemispheres": links
+        "latest_tweets": latest_tweets,
+        "facts_table": html_table,
+        "hemispheres_links": links
     }
 
     # Close the browser after scraping
